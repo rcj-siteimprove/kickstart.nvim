07f47005954062f2d27ad9ad43fad5a0956ddfee
Rolf Christian Jørgensen <114920418+rcj-siteimprove@users.noreply.github.com>
2024-11-04 09:25:07 +0100

    Configure formatter for JS and TS

--- init.lua
+++ init.lua
@@ -704,7 +704,8 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
